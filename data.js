81bc2ac44922e3b913cb1cb644a4672b6c0cad26
StasMelnykCGI <stasmelnik92@gmail.com>
2024-08-12 18:39:28 +0300

    Kamerton79

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-g360_021",
+      "name": "G360_02.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6116755782991703,
+        "pitch": 0.1624205238007228,
+        "fov": 1.4083983627516938
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.668830323922684,
+          "pitch": 0.08591804440321837,
+          "rotation": 0,
+          "target": "1-g360_011"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-g360_011",
+      "name": "G360_01.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.628818766839193,
+          "pitch": 0.08529805105964172,
+          "rotation": 6.283185307179586,
+          "target": "0-g360_021"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
